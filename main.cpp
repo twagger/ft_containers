ab355d0a54b9ff2bbe7a5d5e729c99a7c73f5954
twagner <twagner@student.42.fr>
2022-04-15 15:49:44 +0200

    switching to gtest framework (just initializing)

--- main.cpp
+++ main.cpp
@@ -5,61 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: twagner <twagner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/22 15:53:26 by twagner           #+#    #+#             */
-/*   Updated: 2022/04/12 17:35:27 by twagner          ###   ########.fr       */
+/*   Created: 2022/04/15 15:17:50 by twagner           #+#    #+#             */
+/*   Updated: 2022/04/15 15:48:10 by twagner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <memory>
 #include <algorithm>
+#include <vector>
 #include "containers/Vector.hpp"
 
-int main(void)
-{
-	int i;
+#include <gtest/gtest.h>
 
-	// TEST 1
-	std::cout << std::endl;
-	std::cout << "\033[1;35m ---[ TEST 1 : Create a FT Vector ]--- \033[0m" << std::endl;
-	ft::vector<int>	myVector;
-	myVector.push_back(35);
-	myVector.push_back(42);
-	std::cout << "vector[0] : " << myVector[0] << std::endl;
-	std::cout << "vector[1] : " << myVector[1] << std::endl;
-	
-	// TEST 2
-	std::cout << std::endl;
-	std::cout << "\033[1;35m ---[ TEST 2 : Create a FT Vector by copy ]--- \033[0m" << std::endl;
-	ft::vector<int>	myCopiedVector(myVector);
-	myCopiedVector.push_back(38);
-	myCopiedVector.push_back(49);
-	std::cout << "creation : OK" << std::endl;
+class VectorTest : public ::testing::Test {
+	protected:
+		ft::vector<int>	MyVector;
+		std::vector<int> StdVector;
+};
 
-	// TEST 3
-	std::cout << std::endl;
-	std::cout << "\033[1;35m ---[ TEST 3 : Use iterators on FT Vector ]--- \033[0m" << std::endl;
-	i = 0;
-	for (ft::vector<int>::iterator it = myCopiedVector.begin(); it < myCopiedVector.end(); ++it)
-	{
-		std::cout << "vector[" << i++ << "] : " << *it << std::endl;
-	}
-	
-	// TEST 4
-	std::cout << std::endl;
-	std::cout << "\033[1;35m ---[ TEST 4 : Use reverse iterators on FT Vector ]--- \033[0m" << std::endl;
-	ft::vector<int>::iterator it(myCopiedVector.begin());
-	ft::vector<int>::iterator ite(myCopiedVector.end());
-	ft::vector<int>::reverse_iterator rev_it(ite);
-	ft::vector<int>::reverse_iterator rev_ite(it);
-
-  	std::cout << "MyCopiedVector reversed content :";
-  	while (rev_it != rev_ite)
-    	std::cout << ' ' << *rev_it++;
- 	std::cout << '\n';
-	
-	// END OF PROGRAM
-	std::cout << std::endl;
-	std::cout << "\033[1;35m ---[ End of program ]--- \033[0m" << std::endl;
-	return (0);
+TEST_F(VectorTest, pushBackTest) {
+	MyVector.push_back(42);
+	StdVector.push_back(42);
+	EXPECT_EQ(MyVector[0], StdVector[0]);
 }
